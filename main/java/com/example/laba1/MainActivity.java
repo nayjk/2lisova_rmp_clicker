7f2aef70c90ea8899d6f8dec02a4becba35f9352
Елизавета Лобкова <98636064+LobkovaLiza@users.noreply.github.com>
2022-03-13 16:59:17 +0300

    Update MainActivity.java

--- main/java/com/example/laba1/MainActivity.java
+++ main/java/com/example/laba1/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Integer counter = 0;
+        final String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onClick(View view){
         counter++;
-        TextView counterView = findViewById(R.id.txt_counter);
+               TextView counterView = findViewById(R.id.txt_counter);
         ViewGroup.LayoutParams params = counterView.getLayoutParams();
         counterView.setText(counter.toString());
         if (counter >= 1000){
@@ -24,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
             params.height = 300;
             counterView.setLayoutParams(params);
         }
+    }
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.containsKey("Count")) {
+            counter = savedInstanceState.getInt("Count");
+        }
+        Log.d(LOG_TAG, "onRestoreInstanceState");
+    }
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Count", counter);
+        Log.d(LOG_TAG, "onSaveInstateState");
+    }
+    public void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
+        Log.d(LOG_TAG, "resetUI");
+    }
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+        resetUI();
     }
 }
